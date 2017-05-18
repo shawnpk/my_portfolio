66b2ca668287eface26abc8599976bb2228b4c16
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-17 20:07:18 -0700

    Configure custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
+  get 'about-me', to: 'pages#about'
   get 'pages/contact'
-
   resources :blogs
+  root 'pages#home'
 end
