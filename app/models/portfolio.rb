359dc0888b6f8f9e26849f3a33289aa74bbb42cf
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-19 13:53:24 -0700

    Add validations for blogs, skills and portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,9 @@
 class Portfolio < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :main_image, presence: true
+  validates :thumb_image, presence: true
 end
