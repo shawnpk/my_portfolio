774156af042ee974d71dcedfae193d3bcf80be7e
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-20 08:32:32 -0700

    Add defaults for portfolio images

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,15 +1,22 @@
 class Portfolio < ApplicationRecord
-  extend FriendlyId
-  friendly_id :title, use: :slugged
+  after_initialize :set_defaults
 
   validates :title, presence: true
   validates :body, presence: true
   validates :main_image, presence: true
   validates :thumb_image, presence: true
 
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+
   def self.angular
     where(subtitle: 'Angular')
   end
 
-  scope :ruby_on_rails, -> { where(subtitle: 'Ruby on Rails') }
+  def set_defaults
+    self.main_image ||= 'http://placehold.it/600x400'
+    self.thumb_image ||= 'http://placehold.it/350x200'
+  end
 end
