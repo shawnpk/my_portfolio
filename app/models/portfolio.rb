76f2e28c57adca399f7b91bdd8e7b14ab9925582
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-22 07:11:39 -0700

    Add concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,6 @@
 class Portfolio < ApplicationRecord
+  include Placeholder
+
   after_initialize :set_defaults
 
   validates :title, presence: true
@@ -16,7 +18,7 @@ class Portfolio < ApplicationRecord
   end
 
   def set_defaults
-    self.main_image ||= 'http://placehold.it/600x400'
-    self.thumb_image ||= 'http://placehold.it/350x200'
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
 end
