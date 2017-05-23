a844f687fa193fc048827107d8673e7fed54a21e
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-23 13:49:07 -0700

    Add navbar and default header content

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
-  get 'pages/contact'
+  get 'contact', to: 'pages#contact'
 
   resources :blogs do
     member do
