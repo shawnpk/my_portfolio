40e67aed492cc7ddf3dcf8c9ebbbb59aa1e67442
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-23 10:39:27 -0700

    Move session variable storage to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
-  include DeviseWhitelist
-
   protect_from_forgery with: :exception
 
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  include DeviseWhitelist
+  include SetSource
 end
