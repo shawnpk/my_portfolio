afe7a86925c2ee491755c8e10fe8a74576dc66e5
Shawn Kearney <shawnpk@ineptsoftware.com>
2017-05-25 15:32:54 -0700

    Add spk_view_tool gem for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag :p, greeting, class: 'source-greeting'
     end
   end
+
+  def copyright_generator
+    SpkViewTool::Renderer.copyright 'Shawn Kearney', 'All rights reserved'
+  end
 end
